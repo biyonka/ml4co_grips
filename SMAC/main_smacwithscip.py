a2358ef0b7f53e58033899572a28cd2cfb1a2cfc
MelissaAdl <melissa.aidli@outlook.com>
2022-07-11 15:39:48 +0200

    Changed to the default version - SMAC4AC

--- SMAC/main_smacwithscip.py
+++ SMAC/main_smacwithscip.py
@@ -1,19 +1,14 @@
 import os
 import sys
 import inspect
-
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 import utils
 
-
-from smac.facade.smac_bb_facade import SMAC4BB
-from smac.facade.hyperband_facade import HB4AC
-
+from smac.facade.smac_ac_facade import SMAC4AC
 from scenario import scenario
 
 
-#smac = SMAC4BB(scenario=scenario, tae_runner=run_SCIP_with_smac)
-smac = HB4AC(scenario=scenario, tae_runner=utils.run_SCIP_with_smac)
+smac = SMAC4AC(scenario=scenario, tae_runner=utils.run_SCIP_with_smac)
 best_found_config = smac.optimize()

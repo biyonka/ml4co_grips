3d62836d3e2a180d5ebc52d171369aef4fb60ecb
MelissaAdl <melissa.aidli@outlook.com>
2022-07-12 12:17:08 +0200

    Updated structure of scenario and parameters

--- SMAC/scenario.py
+++ SMAC/scenario.py
@@ -2,44 +2,53 @@ import ConfigSpace.hyperparameters as CSH
 import ConfigSpace as CS
 from smac.scenario.scenario import Scenario
 
+class SMACscenario :
+    def __init__(self):
+        # Define configuration space
+        self.hyperparams = [
+            CSH.CategoricalHyperparameter('branching/scorefunc', ['s', 'p', 'q'], default_value='q'),
+            CSH.UniformFloatHyperparameter('branching/scorefac', 0, 1, default_value=0.167),
+            CSH.CategoricalHyperparameter('branching/preferbinary', [True, False], default_value=False),
+            CSH.UniformFloatHyperparameter('branching/clamp', 0, 0.5, default_value=0.2),
+            CSH.UniformFloatHyperparameter('branching/midpull', 0, 1, default_value=0.75),
+            CSH.UniformFloatHyperparameter('branching/midpullreldomtrig', 0, 1, default_value=0.5),
+            CSH.CategoricalHyperparameter('branching/lpgainnormalize', ['d', 'l', 's'], default_value='s'),
+            CSH.CategoricalHyperparameter('lp/pricing', ['l', 'a', 'f', 'p', 's', 'q', 'd'], default_value='l'),
+            CSH.UniformIntegerHyperparameter('lp/colagelimit', -1, 2147483647, default_value=10),
+            CSH.UniformIntegerHyperparameter('lp/rowagelimit', -1, 2147483647, default_value=10),
+            CSH.CategoricalHyperparameter('nodeselection/childsel', ['d', 'u', 'p', 'i', 'l', 'r', 'h'],
+                                          default_value='h'),
+            CSH.UniformFloatHyperparameter('separating/minortho', 0, 1, default_value=0.9),
+            CSH.UniformFloatHyperparameter('separating/minorthoroot', 0, 1, default_value=0.9),
+            CSH.UniformIntegerHyperparameter('separating/maxcuts', 0, 2147483647, default_value=100),
+            CSH.UniformIntegerHyperparameter('separating/maxcutsroot', 0, 2147483647, default_value=2000),
+            CSH.UniformIntegerHyperparameter('separating/maxroundsroot', -1, 2147483647, default_value=-1),
+            CSH.UniformFloatHyperparameter('separating/minefficacyroot', 0, 1e+98, default_value=0.0001),
+            CSH.UniformIntegerHyperparameter('separating/cutagelimit', -1, 2147483647, default_value=80),
+            CSH.UniformIntegerHyperparameter('separating/poolfreq', -1, 65534, default_value=10),
+            CSH.UniformIntegerHyperparameter('presolving/maxrounds', -1, 2147483647, default_value=-1),
+            CSH.UniformFloatHyperparameter('presolving/abortfac', 0, 1, default_value=0.0008),
+            CSH.UniformIntegerHyperparameter('presolving/maxrestarts', -1, 2147483647, default_value=-1)
+        ]
+        self.configspace = CS.ConfigurationSpace()
 
-#Define configuration space
-hyperparams=[
-CSH.CategoricalHyperparameter('branching/scorefunc', ['s', 'p', 'q'],default_value='q'),
-CSH.UniformFloatHyperparameter('branching/scorefac', 0, 1,default_value=0.167),
-CSH.CategoricalHyperparameter('branching/preferbinary', [True, False],default_value=False),
-CSH.UniformFloatHyperparameter('branching/clamp', 0, 0.5,default_value=0.2),
-CSH.UniformFloatHyperparameter('branching/midpull', 0, 1,default_value=0.75),
-CSH.UniformFloatHyperparameter('branching/midpullreldomtrig', 0, 1,default_value=0.5),
-CSH.CategoricalHyperparameter('branching/lpgainnormalize', ['d','l','s'],default_value='s'),
-CSH.CategoricalHyperparameter('lp/pricing', ['l','a','f','p','s','q','d'],default_value='l'),
-CSH.UniformIntegerHyperparameter('lp/colagelimit', -1, 2147483647,default_value=10),
-CSH.UniformIntegerHyperparameter('lp/rowagelimit', -1, 2147483647,default_value=10),
-CSH.CategoricalHyperparameter('nodeselection/childsel',['d','u','p','i','l','r','h'],default_value='h'),
-CSH.UniformFloatHyperparameter('separating/minortho', 0, 1,default_value=0.9),
-CSH.UniformFloatHyperparameter('separating/minorthoroot', 0, 1,default_value=0.9),
-CSH.UniformIntegerHyperparameter('separating/maxcuts', 0, 2147483647,default_value=100),
-CSH.UniformIntegerHyperparameter('separating/maxcutsroot', 0, 2147483647,default_value=2000),
-CSH.UniformIntegerHyperparameter('separating/maxroundsroot', -1, 2147483647,default_value=-1),
-CSH.UniformFloatHyperparameter('separating/minefficacyroot', 0, 1e+98,default_value=0.0001),
-CSH.UniformIntegerHyperparameter('separating/cutagelimit', -1, 2147483647,default_value=80),
-CSH.UniformIntegerHyperparameter('separating/poolfreq', -1, 65534,default_value=10),
-CSH.UniformIntegerHyperparameter('presolving/axrounds', -1, 2147483647,default_value=-1),
-CSH.UniformFloatHyperparameter('presolving/abortfac', 0, 1,default_value=0.0008),
-CSH.UniformIntegerHyperparameter('presolving/maxrestarts', -1, 2147483647,default_value=-1)
-]
+    def set_configSpace(self) :
+        for hp in self.hyperparams:
+            self.configspace.add_hyperparameter(hp)
 
-configspace = CS.ConfigurationSpace()
-for hp in hyperparams :
-    configspace.add_hyperparameter(hp)
+    def get_configSpace(self):
+        return self.configspace
 
+    # Provide meta data for the optimization
+    def get_scenario(self) :
+        self.set_configSpace()
+        scenario = Scenario({
+            "run_obj": "quality",  # Optimize quality (alternatively runtime)
+            "cs": self.get_configSpace(),
+            "output_dir" : "SMAC3_output",
+            "train_inst_fn" : "1_instances_path_train.txt",
+            "test_inst_fn": "1_instances_path_test.txt",
+            "algo_runs_timelimit" : 180 * 42 * 40 + 180  #3 min * 42 seed * 40 test instances + 3 min
+        })
+        return scenario
 
-# Provide meta data for the optimization
-scenario = Scenario({
-    "run_obj": "quality",  # Optimize quality (alternatively runtime)
-    "runcount-limit": 10,  # Max number of function evaluations (the more the better)
-    "cs": configspace,
-    "train_inst_fn" : "instance_path_train.txt",
-    "test_inst_fn": "instance_path_test.txt",
-    "algo_runs_timelimit" : 60
-})

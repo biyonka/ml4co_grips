513e40e407b6f93e598ef98614a3040fb049a5ee
MelissaAdl <melissa.aidli@outlook.com>
2022-07-12 12:18:03 +0200

    updated main with output file

--- SMAC/main_smacwithscip.py
+++ SMAC/main_smacwithscip.py
@@ -5,10 +5,13 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 import utils
-
 from smac.facade.smac_ac_facade import SMAC4AC
-from scenario import scenario
+from scenario import SMACscenario
 
 
-smac = SMAC4AC(scenario=scenario, tae_runner=utils.run_SCIP_with_smac)
+smac = SMAC4AC(scenario=SMACscenario().get_scenario(), tae_runner=utils.run_SCIP_with_smac)
 best_found_config = smac.optimize()
+print("Best found config :", best_found_config)
+file = open("best_found_config.txt", "w")
+file.write(str(best_found_config))
+file.close()

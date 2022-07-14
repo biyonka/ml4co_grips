361963dfd456918fdace55af98a7189b7f4baabb
aczhang777 <aczhang777@gmail.com>
2022-07-14 16:02:10 +0200

    fixed bugs in evaluation file

--- eval.py
+++ eval.py
@@ -5,9 +5,10 @@ Created on Mon Jul 11 09:37:16 2022
 
 @author: M. Aidli, B. Liang, E. Vercesi, A. Zhang
 """
-## evaluates a bunch of configurations on an instance and save results to a csv file
+## evaluates a bunch of configurations on a bunch of instances and save results to a csv file
 import csv
 import numpy as np
+#import os
 
 from utils import SCIP
 from utils import Log
@@ -25,6 +26,11 @@ def evaluation(instances, configurations, seeds, procedure, time_limit):
     seeds: list of seeds for SCIP
     procedure: string. example: "default", "hyperband"
     time_limit: int time limit for running SCIP (in seconds)
+    
+    Returns
+    ---------
+    A CSV file where each row is the result of running an instance with one of the configurations
+    over all of the inputted seeds
     '''
     # results_as_dict = {}
     # find some way to save configuration too with the remaining results: have one method down below that could work...
@@ -32,22 +38,31 @@ def evaluation(instances, configurations, seeds, procedure, time_limit):
     configs_as_dict = []
     for config in configurations:
         configs_as_dict.append(Configuration.get_dictionary(config))
-    
-    header = ["Instance", "Configuration Procedure", "Primal Dual Integral Percentage Average", 
-             "Primal Dual Integral Percentage Standard Deviation"]
-    name_of_file = procedure + " Results"
-    with open(name_of_file, "w", newline = '') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
+        
     inst_file = open(instances, "r")
-    for line in inst_file:
+    lines = inst_file.readlines()
+    inst_file.close()
+    
+    just_these_lines = lines[:2]
+        
+    #print(lines)
+    
+    header = ["Instance", "Configuration_Procedure", "Primal_Dual_Integral_Percentage_Average", 
+          "Primal_Dual_Integral_Percentage_Standard_Deviation"]
+    name_of_file = procedure + "_Results"
+    file = open(name_of_file, "w", newline = "")
+    writer = csv.writer(file)
+    writer.writerow(header)               
+                     
+    for line in just_these_lines:
+        path = line.rstrip("\n")
         for configuration in configs_as_dict:
             pdi_percentage = []
             for seed in seeds:
                 scip = SCIP()
                 scip.write_parameter_file(D = configuration, timelimit = time_limit)
-                scip.run(path = line, logfile = "results.log", seed = seed) # error here: cannot find results.log
-                l = Log(path = "results.log")
+                scip.run(path = path, seed = seed) # error here: cannot find results.log
+                l = Log(path = "logfile.log.gz")
                 # results_as_dict[self.instance + "_Seed_" + str(seed) + "_Results"] = l.parse()
                 # results_as_dict[self.instance + " Seed " + str(seed) + " Results"] = l.parse().update(configuration)
             ## compute average and standard deviation across seeds for primal-dual integral percentage
@@ -56,9 +71,11 @@ def evaluation(instances, configurations, seeds, procedure, time_limit):
             average_across_seeds = np.average(pdi_percentage)
             std_across_seeds = np.std(pdi_percentage, ddof = 1)
             ## now add to CSV file!
-            name = line.split("/")[2].split(".")[0]
+            name = path.split("/")[-1].split(".")[0]
             data = [name, procedure, average_across_seeds, std_across_seeds]
             writer.writerow(data)
+            #print("1 row added to CSV file")
+    file.close()
             
             
 #TESTING
@@ -83,18 +100,20 @@ CSH.UniformIntegerHyperparameter('separating/maxroundsroot', -1, 2147483647,defa
 CSH.UniformFloatHyperparameter('separating/minefficacyroot', 0, 1e+98,default_value=0.0001),
 CSH.UniformIntegerHyperparameter('separating/cutagelimit', -1, 2147483647,default_value=80),
 CSH.UniformIntegerHyperparameter('separating/poolfreq', -1, 65534,default_value=10),
-CSH.UniformIntegerHyperparameter('presolving/axrounds', -1, 2147483647,default_value=-1),
+CSH.UniformIntegerHyperparameter('presolving/maxrounds', -1, 2147483647,default_value=-1),
 CSH.UniformFloatHyperparameter('presolving/abortfac', 0, 1,default_value=0.0008),
 CSH.UniformIntegerHyperparameter('presolving/maxrestarts', -1, 2147483647,default_value=-1)
 ]
 cs.add_hyperparameters(hyperparams)
 
-sample_hp = cs.sample_configuration()
+sample_hp1 = cs.sample_configuration()
+sample_hp2 = cs.sample_configuration()
 
-filepath = "/home/azhang/ml4co_grips/SMAC/instance_path" # how do you read in files?????
+filepath = "SMAC/1_instances_path_test.txt" # how do you read in files?????
 
-evaluation(instances = filepath, configurations = [sample_hp], seeds = [42, 43], procedure = "test configuration", time_limit = 420)
+evaluation(instances = filepath, configurations = [sample_hp1, sample_hp2], seeds = [42, 43, 44], procedure = "test_configuration", time_limit = 60)
 
-
+# import os
+# os.listdir("/home/azhang/ml4co_grips/SMAC")
+# os.getcwd()
             
-    
